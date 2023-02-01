5668945adb662c3fd9d6a7e9791b922d86ff62fe
khalil_farashiani <khalilfarashi2000@gmail.com>
2023-02-02 00:55:02 +0330

    add some simple method for products  entities

--- internals/domain/product/product.go
+++ internals/domain/product/product.go
@@ -3,6 +3,7 @@ package product
 type Product struct {
 	ID          int
 	Title       string
+	TitleFa     string
 	Description string
 	Price       int
 	Rating      int
@@ -11,4 +12,32 @@ type Product struct {
 	LocationID  int
 	BrandID     int
 	Stock       int
+}
+
+func (p *Product) IncreaseStock(count int) {
+	p.Stock += count
+}
+
+func (p *Product) DecreaseStock(count int) {
+	p.Stock -= count
+}
+
+func (p *Product) UpdateRating(rating int) {
+	p.Rating = rating
+}
+
+func NewProduct(id int, titleFa string, titleEn string, description string, price int, rating int, categoryID int, vendorID int, locationId int, brandID int, stock int) *Product {
+	return &Product{
+		ID:          id,
+		TitleFa:     titleFa,
+		Title:       titleEn,
+		Description: description,
+		Price:       price,
+		Rating:      rating,
+		CategoryID:  categoryID,
+		VendorID:    vendorID,
+		LocationID:  locationId,
+		BrandID:     brandID,
+		Stock:       stock,
+	}
 }
